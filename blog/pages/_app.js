a710ee4d82203c939bbad974b8ce15ac55fddd8c
zjuiuczy <zhaoyuc2@illinois.edu>
2020-07-10 12:56:14 -0500

    header

--- blog/pages/_app.js
+++ blog/pages/_app.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import App from 'next/app'
 import 'antd/dist/antd.css'
 import '../static/style/pages/comm.css'
--- blog/pages/index.js
+++ blog/pages/index.js
@@ -1,11 +1,15 @@
 import Head from 'next/head'
 import React from 'react'
+import {Button} from 'antd'
 
 const Home = () =>(
   <div>
     <Head>
       <title>Home</title>
     </Head>
+    <div>
+      <Button>Submit</Button>
+    </div>
   </div>
 )
 
