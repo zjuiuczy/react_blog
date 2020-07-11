1a101e7b70e569adeb71afececffdb9724748ae1
zjuiuczy <zhaoyuc2@illinois.edu>
2020-07-11 12:29:33 -0500

    react markdown

--- blog/pages/detailed.js
+++ blog/pages/detailed.js
@@ -12,25 +12,46 @@ import {
   FolderOpenOutlined,
   FireOutlined
 } from '@ant-design/icons';
-const Detailed = () =>{
-  <div>
-    <Head>
-      <title>Detailed</title>
-    </Head>
-    <Header />
-    <Row className="comm-main" type="flex" justify="center">
-        <Col className="comm-left" xs={24} sm={24} md={16} lg={18} xl={14}  >
-          Left
-        </Col>
+import ReactMarkdown from 'react-markdown'
 
-        <Col className="comm-right" xs={0} sm={0} md={7} lg={5} xl={4}>
-          Right
-        </Col>
-      </Row>
-  </div>
+const Detailed = () =>{
+  let markdown='# P01:课程介绍和环境搭建\n' +
+  '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
+  '> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
+   '**这是加粗的文字**\n\n' +
+  '*这是倾斜的文字*`\n\n' +
+  '***这是斜体加粗的文字***\n\n' +
+  '~~这是加删除线的文字~~ \n\n'+
+  '\`console.log(111)\` \n\n'+
+  '# p02:来个Hello World 初始Vue3.0\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n'+
+  '***\n\n\n' +
+  '# p03:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '# p04:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '#5 p05:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '# p06:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '# p07:Vue3.0基础知识讲解\n' +
+  '> aaaaaaaaa\n' +
+  '>> bbbbbbbbb\n' +
+  '>>> cccccccccc\n\n'+
+  '``` var a=11; ```'
 
 return (
-  <>
+  <div>
     <Head>
       <title>Detail Page</title>
     </Head>
@@ -58,7 +79,10 @@ return (
               </div>
 
               <div className="detailed-content" >
-                详细内容，下节课编写
+                <ReactMarkdown 
+                  source={markdown} 
+                  escapeHtml={false}  
+                />
               </div>
 
            </div>
@@ -73,7 +97,7 @@ return (
       </Row>
       <Footer/>
 
-   </>
+   <div />
   )
 }
 
