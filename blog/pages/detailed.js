a9fdb21ce8e52ea2a6d225cf086896d7afdec1ac
zjuiuczy <zhaoyuc2@illinois.edu>
2020-07-11 02:18:06 -0500

    02:18

--- blog/pages/detailed.js
+++ blog/pages/detailed.js
@@ -1,8 +1,18 @@
+import React,{useState} from 'react'
 import Head from 'next/head'
-import React from 'react'
-import {Row, Col} from 'antd'
+import {Row, Col , Icon ,Breadcrumb  } from 'antd'
+
 import Header from '../components/Header'
-const Detailed = () =>(
+import Author from '../components/Author'
+import Advert from '../components/Advert'
+import Footer from '../components/Footer'
+import '../static/style/pages/detailed.css'
+import {
+  CalendarOutlined,
+  FolderOpenOutlined,
+  FireOutlined
+} from '@ant-design/icons';
+const Detailed = () =>{
   <div>
     <Head>
       <title>Detailed</title>
@@ -18,6 +28,53 @@ const Detailed = () =>(
         </Col>
       </Row>
   </div>
-)
+
+return (
+  <>
+    <Head>
+      <title>Detail Page</title>
+    </Head>
+    <Header />
+    <Row className="comm-main" type="flex" justify="center">
+      <Col className="comm-left" xs={24} sm={24} md={16} lg={18} xl={14}  >
+          <div>
+            <div className="bread-div">
+              <Breadcrumb>
+                <Breadcrumb.Item><a href="/">首页</a></Breadcrumb.Item>
+                <Breadcrumb.Item>视频列表</Breadcrumb.Item>
+                <Breadcrumb.Item>xxxx</Breadcrumb.Item>
+              </Breadcrumb>
+            </div>
+
+           <div>
+              <div className="detailed-title">
+              React实战视频教程-技术胖Blog开发(更新08集)
+              </div>
+
+              <div className="list-icon center">
+                <span><CalendarOutlined /> 2020-07-10</span>
+                <span><FolderOpenOutlined /> 视频教程</span>
+                <span><FireOutlined /> 5000人</span>
+              </div>
+
+              <div className="detailed-content" >
+                详细内容，下节课编写
+              </div>
+
+           </div>
+
+          </div>
+      </Col>
+      <Col className="comm-right" xs={0} sm={0} md={7} lg={5} xl={4}>
+          <Author />
+          <Advert />
+
+        </Col>
+      </Row>
+      <Footer/>
+
+   </>
+  )
+}
 
 export default Detailed
